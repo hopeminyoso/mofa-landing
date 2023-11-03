a93c35a5f1f823c138a23619d74572a8ad5bcc98
Hope Shirley <77952833+hopeminyoso@users.noreply.github.com>
2023-11-03 15:29:42 +0300

    basic landing page

---
+++ src/components/Header.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './Header.css';
+
+import FlagImage from '../components/assets/Flag_of_Kenya.gif';
+import CoatOfArmsImage from '../components/assets/Coat_of_arms.png';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="corner-flags">
+        <img src={FlagImage} alt="Flag" className="flag" />
+      </div>
+      <div className="header-content">
+        <img src={CoatOfArmsImage} alt="Coat of Arms" className="coat-of-arms" />
+        <h1>Ministry of Foreign Affairs</h1>
+      </div>
+    </header>
+  );
+};
+
+export default Header
+
+
+
+
+
+
---
+++ src/components/MainSection.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './MainSection.css';
+
+const MainSection = () => {
+  return (
+    <section className="main-section">
+      <h1>State Department of Foreign Affairs</h1>
+      <h2>Welcome to the Meeting</h2>
+    </section>
+  );
+};
+
+export default MainSection;
+
+
+
