b5ed38cda358ce41000a3f0e248955d05028f22d
Hope Shirley <77952833+hopeminyoso@users.noreply.github.com>
2023-11-03 15:48:55 +0300

    font changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,7 +12,7 @@ const Header = () => {
       </div>
       <div className="header-content">
         <img src={CoatOfArmsImage} alt="Coat of Arms" className="coat-of-arms" />
-        <h1>Ministry of Foreign Affairs</h1>
+        <h1>Ministry of Foreign and Diaspora Affairs</h1>
       </div>
     </header>
   );
